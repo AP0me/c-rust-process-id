6c630beb236400f2947c79f509e4d06bedcd2b4d
AP0me <anar.abdullazada@ufaz.az>
2025-12-21 18:17:18 +0400

    linux, windows support

---
+++ linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mkdir -p temp
+
+# 1. Rust to LLVM-IR
+rustc +1.81 --emit=llvm-ir --crate-type=staticlib \
+    -C lto=fat -C embed-bitcode=yes \
+    main.rs -o temp/rd_main.ll --target=x86_64-unknown-linux-gnu
+
+# 2. C to LLVM-IR
+# Ensure you have the linux headers available if cross-compiling
+clang -S -emit-llvm main.c -o temp/c_main.ll --target=x86_64-unknown-linux-gnu
+
+# 3. Link IR
+llvm-link temp/c_main.ll temp/rd_main.ll -S -o temp/merged.ll
+
+# 4. Final Compile & Link
+clang temp/merged.ll -o my_app \
+    --target=x86_64-unknown-linux-gnu \
+    -lpthread -ldl -lm -lrt
---
+++ win.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mkdir -p temp
+
+# 1. Rust to LLVM-IR
+rustc +1.81 --emit=llvm-ir --crate-type=staticlib \
+    -C lto=fat -C embed-bitcode=yes \
+    main.rs -o temp/rd_main.ll --target=x86_64-pc-windows-gnu
+
+# 2. C to LLVM-IR
+clang -S -emit-llvm main.c -o temp/c_main.ll --target=x86_64-pc-windows-gnu
+
+# 3. Link IR
+llvm-link temp/c_main.ll temp/rd_main.ll -S -o temp/merged.ll
+
+# 4. Final Compile & Link
+# Note: We include common Windows libs required by the Rust runtime
+clang temp/merged.ll -o my_app.exe \
+    --target=x86_64-pc-windows-gnu \
+    -lntdll -lsynchronization -luser32 -lkernel32 -lws2_32 -lbcrypt
